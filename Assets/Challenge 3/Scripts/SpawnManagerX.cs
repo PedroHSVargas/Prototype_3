4dcac1e8de36cf8d97b5541d5ab0718fb362bb0b
Pedro Vargas <pedroh_vargas@hotmail.com>
2025-11-02 20:32:03 -0300

    Spawn objectPrefabs are fixed

--- Assets/Challenge 3/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 3/Scripts/SpawnManagerX.cs
@@ -7,13 +7,12 @@ public class SpawnManagerX : MonoBehaviour
     public GameObject[] objectPrefabs;
     private float spawnDelay = 2;
     private float spawnInterval = 1.5f;
-
     private PlayerControllerX playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("PrawnsObject", spawnDelay, spawnInterval);
+        InvokeRepeating("SpawnObjects", spawnDelay, spawnInterval);
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
     }
 
